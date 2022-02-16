34eeec657a4c23a16a015ca67ba524e1f18ef3ba
Alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2022-02-16 20:12:29 +0300

    editing git ignore

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app     = express();
-const port    = process.env.PORT|| 7000;
+const port    = process.env.PORT|| 7100;
 
 app.use(express.static(__dirname + '/public'));
 
