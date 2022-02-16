0e117ed67a1062a6e3126f3b02e097f4d1829207
Alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2022-02-16 19:56:08 +0300

    chaging index.js name to app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app     = express();
+const port    = process.env.PORT|| 7000;
+
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+
+// any page dose not exist
+app.use(function(req,res) {
+    res.status(404).render('error-404');
+});
+
+app.listen(port);
+console.log('Server is listening on port ' + port);
